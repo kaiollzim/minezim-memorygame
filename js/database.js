25ed35fc75b18cafff04dee074a22ccbd9a48909
kaiollzim <kaiolucasara@gmail.com>
2022-07-31 18:35:14 -0300

    sss

--- js/database.js
+++ js/database.js
@@ -1,20 +1,20 @@
-var express=require("express");
-var bodyParser=require("body-parser");
+// var express=require("express");
+// var bodyParser=require("body-parser");
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 
-var db = mongoose.connection;
+// var db = mongoose.connection;
 
-db.on('error', console.log.bind(console, "[-] Connection error"));
-db.once('open', (callback) => {
-    console.log("[+] Connection succeeded")
-})
+// db.on('error', console.log.bind(console, "[-] Connection error"));
+// db.once('open', (callback) => {
+//     console.log("[+] Connection succeeded")
+// })
 
-var app=express()
+// var app=express()
 
-app.use(bodyParser.json());
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+// app.use(bodyParser.json());
+// app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }))
